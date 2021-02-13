1f6524bd05075272647f554e1ba81e6b362d27b7
vladicomescu@gmail.com <EvD-1hb-21w-Efk>
2021-02-13 09:23:57 +0200

    commit inicial

--- shortner/views.py
+++ shortner/views.py
@@ -13,11 +13,11 @@ def create(request):
     if request.method == 'POST':
         link = request.POST['link']
         uid = str(uuid.uuid4())[:5]
-        new_url = Url(link=link,uuid=uid)
+        new_url = Url(link=link, uuid=uid)
         new_url.save()
         return HttpResponse(uid)
 
 
 def go(request, pk):
     url_details = Url.objects.get(uuid=pk)
-    return redirect('https://'+url_details.link)
+    return redirect('https://' + url_details.link)
